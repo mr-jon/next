de16bfb183884a224cc61e5d6733dd31a21e3dea
JON <jon@JONs-MacBook-Pro.local>
2021-01-30 15:35:30 -0400

    Adicionando delay

--- pages/tempo.js
+++ pages/tempo.js
@@ -6,13 +6,16 @@ function Tempo(props){
 	return (
 		<div>
 			<div>{dynamicDateString} (dinâmico)</div>
-			<div>{props.staticDateString} (estático)</div>
+			<div>{props.staticDateString} (estático - com delay)</div>
 		</div>
 	)
 }
 
-export function getStaticProps(){
+export async function getStaticProps(){
 	console.log('> Passando pelo getStaticProps()')
+	console.log('> Adicionando delay de 5 segundos')
+
+	await delay(5000)
 	const staticDate = new Date()
 	const staticDateString = staticDate.toGMTString()
 
@@ -21,6 +24,8 @@ export function getStaticProps(){
 			staticDateString
 		}
 	}
-} 
+}
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 export default Tempo

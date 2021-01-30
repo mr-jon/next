8bbb0971c2b5e57823804d77c74a0af8ac87e023
JON <jon@JONs-MacBook-Pro.local>
2021-01-29 23:11:40 -0400

    Teste de Link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from 'next/link'
+
 function Home(){
-	return <h1>Página Inicial</h1>
+	return (
+		<div>
+			<h1>Página Inicial</h1>
+			<Link href="/sobre">
+				<a>Sobre</a>
+			</Link>
+		</div>
+	)
 }
 
 export default Home

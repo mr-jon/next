236fb8a98024d5f26d512bcb32344312c38adbcf
JON <jon@JONs-MacBook-Pro.local>
2021-01-29 22:06:54 -0400

    Nova home

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,5 @@
-import { useState } from 'react'
-
 function Home(){
-	return (
-		<div>
-			<h1>Hello World! Let's Go!</h1>
-			<Count />
-		</div>
-	)
-}
-
-function Count(){
-	const [counter, setCounter] = useState(1)
-
-	function addCounter(){
-		setCounter(counter+1)
-	}
-	return (
-		<div>
-			<div>{counter}</div>
-			<button onClick={addCounter}>Adicionar</button>
-		</div>
-	)
+	return <h1>Hello World! Let's Go!</h1>
 }
 
 export default Home
